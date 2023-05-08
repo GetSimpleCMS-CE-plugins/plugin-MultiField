0d13e1ddfe8a41a50e6835fcec5bb2073b4447f6
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2023-05-08 21:35:29 +0200

    1.0

---
+++ multiField/lang/en_US.php
@@ -0,0 +1,37 @@
+<?php
+
+$i18n = array(
+
+	"SETTINGS" => "MultiField Settings",
+	"DESCRIPTION" => "Creator for Custom Fields for concrete page",
+	"PAYPAL" => "If you want to see new plugins, buy me a ☕ :)",
+	"ADDNEW" => "Add New",
+	"MIGRATE" => "Migrate",
+	"BACKTOLIST" => "Back to list",
+	"MULTIFIELDCREATE" => "MultiField Create",
+	"ADDNEWINPUT" => "Add New Input",
+	"TITLE" => "Title",
+	"SELECTPAGE" => "Select the page on which you want to show the multifields",
+	"MULTIFIELDSLIST" => "MultiFields list",
+	"REMOVEQUESTION" => "are you sure you want delete this item?",
+	"ORDER" => "Change Order",
+	"DONE" => "Done",
+	"SAVE" => "Save Inputs",
+	"GETPHOTO" => "Choose a photo",
+
+	//input
+
+	"TEXT" => "Text",
+	"TEXTAREA" => "Textarea",
+	"WYSYWIG" => "Wysywig",
+	"COLOR" => "Color",
+	"DATE" => "Date",
+	"FOTO" => "Image",
+	"LINK" => "Link",
+
+	//migrate
+
+	"OLDURL" => "Old Url",
+	"NEWURL" => "New Url",
+	"CHANGEURL" => "Change multiFields url",
+);
---
+++ multiField/lang/pl_PL.php
@@ -0,0 +1,37 @@
+<?php
+
+$i18n = array(
+
+	"SETTINGS" => "MultiField Ustawienia",
+	"DESCRIPTION" => "Kreator pól niestandardowych dla konkretnej strony",
+	"PAYPAL" => "Jeśli chcesz zobaczyć nowe wtyczki, kup mi ☕ :)",
+	"ADDNEW" => "Dodaj nowe",
+	"MIGRATE" => "Migracja",
+	"BACKTOLIST" => "Powrót do listy",
+	"MULTIFIELDCREATE" => "Utwórz MultiField",
+	"ADDNEWINPUT" => "Dodaj nowe pole",
+	"TITLE" => "Tytuł",
+	"SELECTPAGE" => "Wybierz stronę, na której chcesz wyświetlić Multifield",
+	"MULTIFIELDSLIST" => "Lista MultiField",
+	"REMOVEQUESTION" => "Jesteś pewien, że chcesz usunąć ten element?",
+	"ORDER" => "Zmień Kolejność",
+	"DONE" => "Gotowe",
+	"SAVE" => "Zapisz Pola",
+	"GETPHOTO" => "Wybierz zdjęcie",
+
+	//input
+
+	"TEXT" => "Text",
+	"TEXTAREA" => "Treść",
+	"WYSYWIG" => "Wysywig",
+	"COLOR" => "Kolor",
+	"DATE" => "Data",
+	"FOTO" => "Zdjęcie",
+	"LINK" => "Link",
+
+	//migrate
+
+	"OLDURL" => "Stary URL",
+	"NEWURL" => "Nowy URL",
+	"CHANGEURL" => "Zmień URL multiField",
+);
